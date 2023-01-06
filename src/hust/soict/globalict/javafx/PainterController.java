257b6ba82a58b92bea5dc14c5594db96c4ad10ee
ntu002 <ngoctubn2002@gmail.com>
2023-01-06 15:00:24 +0700

    GUIProject

---
+++ src/hust/soict/globalict/javafx/PainterController.java
@@ -0,0 +1,50 @@
+package hust.soict.globalict.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.*;
+
+public class PainterController {
+
+    private Color colorToWrite = Color.BLACK;
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event){
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void onSwitchToPen(ActionEvent event){
+        this.colorToWrite = Color.BLACK;
+    }
+
+    @FXML
+    void onSwitchToEraser(ActionEvent event){
+        this.colorToWrite = Color.WHITE;
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event){
+        Circle newCirle = new Circle(
+                event.getX(),
+                event.getY(),
+                4,
+                this.colorToWrite
+        );
+        drawingAreaPane.getChildren().add(newCirle);
+    }
+
+    @FXML
+    private RadioButton penBtn;
+
+    @FXML
+    private RadioButton eraserBtn;
+}
---
+++ src/hust/soict/globalict/swing/AWTAccumulator.java
@@ -0,0 +1,48 @@
+package hust.soict.globalict.swing;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class AWTAccumulator extends Frame{
+    private TextField tfInput;
+    private TextField tfOutput;
+    private int sum = 0;
+
+    public AWTAccumulator() {
+        // bo cuc khung
+        setLayout(new GridLayout(2,2));
+
+        // them phan dau tien vao khung, nhan dan an
+        add(new Label("Enter an integer: "));
+
+        tfInput = new TextField(10);
+        add(tfInput);
+        tfInput.addActionListener(new TFInputListener());
+
+        add(new Label("The Accumulated Sum is: "));
+
+        tfOutput = new TextField(10);
+        tfOutput.setEditable(false);
+        add(tfOutput);
+
+        setTitle("AWT Accumulator");
+        setSize(350, 120);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AWTAccumulator();
+    }
+
+    private class TFInputListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            int numberIn = Integer.parseInt(tfInput.getText());
+            sum += numberIn;
+            tfInput.setText("");
+            tfOutput.setText(sum + "");
+        }
+    }
+}
